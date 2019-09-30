c0d2a96778539081e42ad56b4e2200bfeca4e220
yaswanth yarram <43011603+yaswnth2397@users.noreply.github.com>
2019-09-30 18:48:53 -0500

    Update mapper.py

--- min_avg/mapper.py
+++ min_avg/mapper.py
@@ -6,7 +6,7 @@ mapoutput = open("mapoutputs.txt", "w")
 for line in mapinputs:
   # separate each column using space
   data = line.strip().split('	')
-  # if number of columns in dataset is 12 then separate state and total discharges columns in datasource and write them in mapoutput.txt
+  # if number of columns in dataset is 12 then separate city and average covered charges columns in datasource and write them in mapoutput.txt
   if (len(data) == 12):
    Definition, Id, Name, StreetAddress, City, State, Zipcode, Refferalregion, Totaldischarges, Averagecoveredcharges,  Averagetotalpayements, Averagemedicarepayments   = data
    mapoutput.write(City + "\t" + Averagecoveredcharges + "\n")
