7c112e139038beb1de7897fe3faa2ef3365b4e1f
Hongyang Wang <s522652@nwmissouri.deu>
2019-09-30 16:32:09 -0500

    find the sum of avage medicare

---
+++ avage_medicare/mapper.py
@@ -0,0 +1,15 @@
+# input open the inpatientcharges and the text 
+input = open("./data/inpatientCharges.txt","r")
+# write results in output.txt
+output = open("map.txt", "w")
+# open everyline in inpatientCharges.txt
+for line in input:
+  # separate each column using space
+  data = line.strip().split('	')
+  # if number of columns in dataset is 12 then separate state and total discharges columns in datasource and write them in output.txt
+  if (len(data) == 12):
+   Definition, Id, Name, StreetAddress, City, State, Zipcode, Refferalregion, Totaldischarges, Averagecoveredcharges,  Averagetotalpayements, Averagemedicarepayments   = data
+   output.write(City + "\t" + Averagemedicarepayments + "\n")
+# close input and output.txt
+input.close()
+output.close()
---
+++ avage_medicare/reducer.py
@@ -0,0 +1,31 @@
+#  open the file mapperoutput.txt and read the file into rinput.
+rinput = open("./avage_medicare/map.txt","r")
+# open the file routput and write the file into redoupt
+routput = open("./avage_medicare/ouput.txt", "w")
+
+thisKey = ""
+thisValue = 0.0
+# Reads the each line in rinput and seperated by tab space
+for line in rinput:
+  data = line.strip().split('\t')
+  City, Averagemedicarepayments = data
+  Averagemedicarepayments = Averagemedicarepayments.replace("$","")
+  Averagemedicarepayments = Averagemedicarepayments.replace(",","")
+  if City != thisKey:
+    if thisKey:
+      # output the last key value pair result
+      routput.write(thisKey + '\t' + str(thisValue)+'\n')
+
+    # start over when changing keys
+    thisKey = City
+    thisValue = 0.0
+  
+  # apply the aggregation function for calculating totaldischarges.
+
+    thisValue += float(Averagemedicarepayments)
+
+# output the final entry into the routput file
+routput.write(thisKey + '\t' + str(thisValue)+'\n')
+# close rinput and routput file
+rinput.close()
+routput.close()
