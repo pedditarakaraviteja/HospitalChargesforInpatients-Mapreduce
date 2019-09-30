5448ee1dc5bcf222efd075a9f40344b20600cab3
pedditarakaraviteja <pedditarakaraviteja@gmail.com>
2019-09-30 12:38:27 -0500

    Created mapper.py and reducer.py for calculating the total discharges

---
+++ total_cal/mapper.py
@@ -0,0 +1,15 @@
+# mapinput open the inpatientcharges and the text 
+mapinput = open("inpatientCharges.txt","r")
+# write results in output.txt
+mapoutput = open("mapperoutput.txt", "w")
+# open everyline in inpatientCharges.txt
+for line in mapinput:
+  # separate each column using space
+  data = line.strip().split('	')
+  # if number of columns in dataset is 12 then separate state and total discharges columns in datasource and write them in mapoutput.txt
+  if (len(data) == 12):
+   Definition, Id, Name, StreetAddress, City, State, Zipcode, Refferalregion, Totaldischarges, Averagecoveredcharges,  Averagetotalpayements, Averagemedicarepayments   = data
+   mapoutput.write(State + "\t" + Totaldischarges + "\n")
+# close mapinput and mapoutput.txt
+mapinput.close()
+mapoutput.close()
---
+++ total_cal/reducer.py
@@ -0,0 +1,29 @@
+#  open the file mapperoutput.txt and read the file into redinput.
+redinput = open("mapperoutput.txt","r")
+# open the file redoutput and write the file into redoupt
+redouput = open("redouput.txt", "w")
+
+thisKey = ""
+thisValue = 0.0
+# Reads the each line in redinput and seperated by tab space
+for line in redinput:
+  data = line.strip().split('\t')
+  State, Totaldischarges = data
+
+  if State != thisKey:
+    if thisKey:
+      # output the last key value pair result
+      redouput.write(thisKey + '\t' + str(thisValue)+'\n')
+
+    # start over when changing keys
+    thisKey = State 
+    thisValue = 0.0
+  
+  # apply the aggregation function for calculating totaldischarges.
+  thisValue += float(Totaldischarges)
+
+# output the final entry into the redouput file
+redouput.write(thisKey + '\t' + str(thisValue)+'\n')
+# close redinput and redouput file
+redinput.close()
+redouput.close()
