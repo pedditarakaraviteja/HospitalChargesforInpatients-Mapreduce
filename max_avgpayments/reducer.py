35976dbac784a98feaeba71b8aa15931597d916f
SAI MANIKANTA DURGA PRASAD VINUKONDA <sai1996.vinukonda@gmail.com>
2019-09-30 20:50:38 -0500

    Add reducer.py

---
+++ max_avgpayments/reducer.py
@@ -0,0 +1,43 @@
+# This file has given a read access
+mapin = open("./max_avgpayments/mapoutputs.txt","r")
+# This file has given an write access 
+mapout = open("./max_avgpayments/r.txt","w")
+thisKey = ""
+thisValue = 0.0
+fullList = []
+
+# Hence in every part of the mapin take the row list as a array and append the value of the state and average total payments and row list 
+for line in mapin:
+  rowList = []
+  data = line.split(' ')
+  State = data[0]
+  Averagetotalpayements = data[1]
+  rowList.append(State)
+  rowList.append(Averagetotalpayements)
+  fullList.append(rowList)
+  # print(fullList)
+# This sort the entire list
+fullList.sort()
+maxValue = 0.0
+finalObject = {}
+for row in fullList:
+  
+  if row[0] != thisKey:
+    thisKey = row[0]
+    thisValue = 0.0
+    maxValue = 0.0
+
+  thisKey = row[0]
+  row[1] = row[1].replace(",","")
+  thisValue = float(row[1])
+  if thisValue > maxValue:
+    maxValue = thisValue
+  finalObject[thisKey] = maxValue
+for keyValue in finalObject:
+  # print(keyValue + " " + str(finalObject[keyValue])+"\n")
+  mapout.write(keyValue + " " + str(finalObject[keyValue]) +"\n")
+
+# close redinput file 
+mapin.close()
+# close the out file 
+mapout.close()
