450b063c672c28f77978d0f21bf546df3329900f
SAI MANIKANTA DURGA PRASAD VINUKONDA <sai1996.vinukonda@gmail.com>
2019-09-30 20:49:27 -0500

    Add mapper.py

--- max_avgpayments/mapper.py
+++ max_avgpayments/mapper.py
@@ -1,15 +1,18 @@
-# mapinput open the inpatientcharges and the text 
+# mapinput open the inpatientcharges and given the read access 
 mapinputs = open("./data/inpatientCharges.txt","r")
-# write results in output.txt
+# write results in output.txt.given the write access 
 mapoutput = open("mapoutputs.txt", "w")
 # open everyline in inpatientCharges.txt
 for line in mapinputs:
-  # separate each column using space
+  # separate each column usi
   data = line.strip().split('	')
   # if number of columns in dataset is 12 then separate state and total discharges columns in datasource and write them in mapoutput.txt
   if (len(data) == 12):
-   Definition, Id, Name, StreetAddress, City, State, Zipcode, Refferalregion, Totaldischarges, Averagecoveredcharges,  Averagetotalpayements, Averagemedicarepayments   = data
-   mapoutput.write(State + "\t" + Averagetotalpayements + "\n")
-# close mapinput and mapoutput.txt
+    Definition, Id, Name, StreetAddress, City, State, Zipcode, Refferalregion, Totaldischarges, Averagecoveredcharges,  Averagetotalpayements, Averagemedicarepayments   = data
+    mapoutput.write(State + "\t" + Averagetotalpayements + "\n")
+# close the mapinputs file
 mapinputs.close()
+# close the mapoutput file 
 mapoutput.close()
+
+
