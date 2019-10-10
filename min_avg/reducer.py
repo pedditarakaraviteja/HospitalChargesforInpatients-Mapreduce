4d6bad396dd7e26b2d55141083a7d74dbac74a3c
Yaswanth <yarram.pegasian@gmail.com>
2019-10-10 17:41:22 -0500

    made some changes in min_avg/reducer.py

--- min_avg/reducer.py
+++ min_avg/reducer.py
@@ -2,3 +2,12 @@
 mapin = open("./min_avg/mapoutputs.txt","r")
 # mapout is a vriable which writes output to r.txt file.
 mapout = open("./min_avg/r.txt","w")
+thisKey = ""
+# thisValue = 0.0
+fullList = []
+for line in mapin:
+  rowList = []
+  data = line.split('\t')
+  City = data[0]
+  Averagecoveredcharges = data[1]
+  # rowList is an array which is appended with city names.
