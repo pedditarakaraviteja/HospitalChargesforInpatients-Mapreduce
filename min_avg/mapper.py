ef3040927bdc31d47496a6d7c37d66f2fe32c052
Yaswanth <yarram.pegasian@gmail.com>
2019-10-10 17:25:03 -0500

    made some changes in min_avg/mapper.py

--- min_avg/mapper.py
+++ min_avg/mapper.py
@@ -6,3 +6,6 @@ for line in mapinputs:
   # separate each column using space
   data = line.strip().split('	')
   # if the number of columns in dataset is 12 then separate city and average covered charges columns in datasource and write them in mapoutput.txt
+  if (len(data) == 12):
+   Definition, Id, Name, StreetAddress, City, State, Zipcode, Refferalregion, Totaldischarges, Averagecoveredcharges,  Averagetotalpayements, Averagemedicarepayments   = data
+   mapoutput.write(City + "\t" + Averagecoveredcharges + "\n")
