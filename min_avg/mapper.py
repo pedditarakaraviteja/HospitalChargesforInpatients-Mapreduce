1b729707b6af3458d39d23314d5e0ad0df9a4163
Yaswanth <yarram.pegasian@gmail.com>
2019-10-10 17:20:48 -0500

    made some changes in mapper.py

--- min_avg/mapper.py
+++ min_avg/mapper.py
@@ -2,3 +2,7 @@
 mapinputs = open("./data/inpatientCharges.txt","r")
 # write results in output.txt
 mapoutput = open("mapoutputs.txt", "w")
+for line in mapinputs:
+  # separate each column using space
+  data = line.strip().split('	')
+  # if number of columns in dataset is 12 then separate city and average covered charges columns in datasource and write them in mapoutput.txt
