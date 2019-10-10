b829f53afafb6853566258d7049baf429bf092a4
Yaswanth <yarram.pegasian@gmail.com>
2019-10-10 17:35:54 -0500

    made some changes in min_avg/reducer.py

---
+++ min_avg/reducer.py
@@ -0,0 +1,4 @@
+# mapin is a variable which reads input file mapoutputs.txt.
+mapin = open("./min_avg/mapoutputs.txt","r")
+# mapout is a vriable which writes output to r.txt file.
+mapout = open("./min_avg/r.txt","w")
