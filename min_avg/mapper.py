77e5f558f16dc345ccc5ed328d1c023a8df16237
Yaswanth <yarram.pegasian@gmail.com>
2019-10-10 17:22:41 -0500

    made some changes  in min_avg/mapper.py

--- min_avg/mapper.py
+++ min_avg/mapper.py
@@ -5,4 +5,4 @@ mapoutput = open("mapoutputs.txt", "w")
 for line in mapinputs:
   # separate each column using space
   data = line.strip().split('	')
-  # if number of columns in dataset is 12 then separate city and average covered charges columns in datasource and write them in mapoutput.txt
+  # if the number of columns in dataset is 12 then separate city and average covered charges columns in datasource and write them in mapoutput.txt
