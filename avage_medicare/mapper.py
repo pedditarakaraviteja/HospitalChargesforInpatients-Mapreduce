0923dd51b8c30073831cc7630b40ce9ffec72ad6
Hongyang Wang <s522652@nwmissouri.deu>
2019-10-11 00:24:08 -0500

    write a for loop to read data by each line. and use code split data by space.

--- avage_medicare/mapper.py
+++ avage_medicare/mapper.py
@@ -3,3 +3,6 @@ input = open("./data/inpatientCharges.txt","r")
 # write results in output.txt
 output = open("./avage_medicare/map.txt", "w")
 # open everyline in inpatientCharges.txt
+for line in input:
+  # separate each column using space
+  data = line.strip().split('	')
