f4859adaae4b671d0ac353d14ff2da5f81efe2c2
Hongyang Wang <s522652@nwmissouri.deu>
2019-10-11 00:34:48 -0500

    crate a reducer.py file and write a code to open input and output files.

---
+++ avage_medicare/reducer.py
@@ -0,0 +1,4 @@
+#  open the file mapperoutput.txt and read the file into rinput.
+rinput = open("./avage_medicare/map.txt","r")
+# open the file routput and write the file into redoupt
+routput = open("./avage_medicare/ouput.txt", "w")
