ccd3f2d91a0eebeadcdc6eda7175cfdbf9456006
Hongyang Wang <s522652@nwmissouri.deu>
2019-10-11 00:37:15 -0500

    set up key and value and write a for loop to read each line. also split data buy tab.

--- avage_medicare/reducer.py
+++ avage_medicare/reducer.py
@@ -2,3 +2,8 @@
 rinput = open("./avage_medicare/map.txt","r")
 # open the file routput and write the file into redoupt
 routput = open("./avage_medicare/ouput.txt", "w")
+thisKey = ""
+thisValue = 0.0
+# Reads the each line in rinput and seperated by tab space
+for line in rinput:
+  data = line.strip().split('\t')
